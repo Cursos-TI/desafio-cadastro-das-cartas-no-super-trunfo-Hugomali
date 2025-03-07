2c908a639e5e60c50e48d754941dbad718235a53
Hugomali <hugolima1988@gmail.com>
2025-03-07 17:42:21 +0000

    criar print f 1

---
+++ xadrez.c
@@ -0,0 +1,49 @@
+criar printf 1
+#include<stdio.h>
+
+int main(){
+
+   char estado;
+   char codigo;
+   char nome  [50];
+   int populacao;
+   float area;
+   float pib;
+   int quantidade;
+
+
+   printf("qual e o estado: \n");
+   scanf("%s",&estado);
+
+
+   printf("qual e numero da carta: \n");
+   scanf("%d",&codigo);
+
+
+   printf("qual e nome da cidade: \n");
+   scanf("%s",&nome);
+
+
+   printf("qual e a população: \n");
+   scanf("%d",&populacao);
+
+
+   printf("qual e a area km: \n");
+   scanf("%f",&area);
+
+
+   printf("qual e o pib: \n");
+   scanf("%f",&pib);
+
+
+   printf("quantos pontos turisticos: \n");
+   scanf("%d",&quantidade);
+
+   return 0;
+
+}
+
+
+
+
+
