14fa9785838f898905b40efe3b6b5820591289c9
Hugomali <hugolima1988@gmail.com>
2025-03-22 16:58:37 +0000

    xadrezz

--- xadrez.c
+++ xadrez.c
@@ -3,14 +3,17 @@
 
 int main(){
 
-   char estado;
+   char estado; 
    char codigo;
-   char nome  [50];
+   char nome  [50]; 
    int populacao;
-   float area;
+   float area; 
    float pib;
-   int quantidade;
+   int quantidade; 
 
+
+   // carta 1 *******
+   printf("carta1\n");
 
    printf("qual e o estado: \n");
    scanf("%s",&estado);
@@ -38,6 +41,43 @@ int main(){
 
    printf("quantos pontos turisticos: \n");
    scanf("%d",&quantidade);
+
+
+
+   // carta 2 *******
+   
+   printf("carta2\n");
+
+   printf("qual e o estado: \n");
+   scanf("%s",&estado);
+
+
+   printf("qual e numero da carta: \n");
+   scanf("%d",&codigo);
+
+
+   printf("qual e nome da cidade: \n");
+   scanf("%s",&nome);
+
+
+   printf("qual e a população: \n");
+   scanf("%d",&populacao);
+
+
+   printf("qual e a area km: \n");
+   scanf("%f",&area);
+
+
+   printf("qual e o pib: \n");
+   scanf("%f",&pib);
+
+
+   printf("quantos pontos turisticos: \n");
+   scanf("%d",&quantidade);
+
+
+
+
 
    return 0;
 
